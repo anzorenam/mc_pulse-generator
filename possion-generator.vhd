29758b19c9eaab2a5fe039274754c85609acbdf7
Marcos Anzorena <anzorena@geofisica.unam.mx>
2022-02-14 12:31:41 +0900

    Update possion-generator.vhd

--- possion-generator.vhd
+++ possion-generator.vhd
@@ -2,19 +2,24 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity event_generator is
-port(gclk,rst: in std_logic;
+entity poisson_generator is
+generic(rate_mask: natural:= 25770;  -- default rate = 1.5 kHz at 250 MHz clk
+init_seed: natural:= 100);
+port(gclk,grst: in std_logic;
 make_event: out std_logic);
 end entity;
-architecture x of event_generator is
+architecture x of poisson_generator is
+constant pvalue: unsigned:= to_unsigned(rate_mask,96);
+constant seed: natural:= init_seed;
 signal lfsr: unsigned(95 downto 0);
-signal urdn: unsigned(31 downto 0);
+signal urnd: unsigned(31 downto 0);
+signal event_aux: std_logic;
 begin
 
-process(gclk,rst)
+process(gclk,grst)
 begin
-  if rst='1' then
-    lfsr<=(5=>'1',others=>'0');
+  if grst='0' then
+    lfsr<=to_unsigned(seed,96);
   else
     if rising_edge(gclk) then
       lfsr(95 downto 32)<=lfsr(63 downto 0);
@@ -25,21 +30,22 @@ begin
   end if;
 end process;
 
-process(gclk,rst)
+process(gclk,grst)
 begin
-  if rst='1' then
-    make_event<='0';
+  if grst='0' then
+    event_aux<='0';
   else
     if rising_edge(gclk) then
-      if urdn<="00000000010000011000100100110111" then
-        make_event<='1';
+      if urnd<=pvalue then
+        event_aux<='1';
       else
-        make_event<='0';
+        event_aux<='0';
       end if;
     end if;
   end if;
 end process;
 
-urdn<=lfsr(31 downto 0);
+make_event<=event_aux;
+urnd<=lfsr(31 downto 0);
 
 end x;
