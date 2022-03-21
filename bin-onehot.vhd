c5bf9210aaa15ac1148402529e175b86ef72ba13
Marcos Anzorena <anzorena@geofisica.unam.mx>
2022-03-21 18:46:09 +0900

    Update bin-onehot.vhd

--- bin-onehot.vhd
+++ bin-onehot.vhd
@@ -4,30 +4,46 @@ use ieee.numeric_std.all;
 
 entity bin_onehot is
 generic(bin_width: natural:= 10;
-onehot_width: natural:= 1024);
+onehot_width: natural:= 128);
 port(gclk: std_logic;
 ctrl_rand: std_logic_vector (2 downto 0);
 binary: in unsigned (bin_width-1 downto 0);
-onehot: out std_logic_vector (onehot_width-1 downto 0));
+onehot: out std_logic_vector (onehot_width-1+8 downto 0));
 end entity;
 
 architecture x of bin_onehot is
-signal decoder: std_logic_vector (onehot_width-1 downto 0);
+signal decoder: std_logic_vector (onehot_width-1+8 downto 0);
+signal bin_msb: unsigned (bin_width-1-3 downto 0);
+signal bin_lsb: unsigned (bin_width-1-7 downto 0);
 
 begin
 
-process(binary)
-variable code: std_logic_vector (onehot_width-1 downto 0);
+process(bin_msb)
+variable code0: std_logic_vector (onehot_width-1 downto 0);
 begin
-  code:=(others=>'0');
+  code0:=(others=>'0');
   for j in 0 to onehot_width-1 loop
-    if to_integer(binary)=j then
-      code(j):='1';
+    if to_integer(bin_msb)=j then
+      code0(j):='1';
     else
-      code(j):='0';
+      code0(j):='0';
     end if;
   end loop;
-  decoder<=code;
+  decoder(onehot_width-1+8 downto 8)<=code0;
+end process;
+
+process(bin_lsb)
+variable code1: std_logic_vector (7 downto 0);
+begin
+  code1:=(others=>'0');
+  for j in 0 to 7 loop
+    if to_integer(bin_lsb)=j then
+      code1(j):='1';
+    else
+      code1(j):='0';
+    end if;
+  end loop;
+  decoder(7 downto 0)<=code1;
 end process;
 
 process(gclk)
@@ -40,5 +56,8 @@ begin
     end if;
   end if;
 end process;
+
+bin_msb<=binary(bin_width-1 downto 3);
+bin_lsb<=binary(2 downto 0);
 
 end x;
